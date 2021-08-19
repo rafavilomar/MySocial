a8eb9e0ced3f9105a73ce5c5cab6328ba01aab0a
Rafael Vilomar <rafavilomar@gmail.com>
2021-08-19 09:33:56 -0400

    response success and error added;

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import response from '../../../network/response.js';
 
 const router = express.Router();
 
-router.get('/', (req, res)=> res.send('Everything is ok.'));
+router.get('/', (req, res)=> {
+  response.success(req, res, 'Everything is ok')
+});
 
 export default router;
---
+++ network/response.js
@@ -0,0 +1,18 @@
+const response = {
+  success : (req, res, message = 'OK', status = 200) => {
+    res.status(status).send({
+      error: false,
+      status: status,
+      body: message
+    })
+  },
+  error : (req, res, message = 'Internal server error', status = 500) => {
+    res.status(status).send({
+      error: false,
+      status: status,
+      body: message
+    })
+  } 
+}
+
+export default response;
